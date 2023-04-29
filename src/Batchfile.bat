e062038a3833133e3e991af8c368bd502bb06438
FadyMilad22 <elfadyy2010@yahoo.com>
2023-04-29 20:58:54 +0300

    adding the Batch file & the arxml test files

---
+++ src/Batchfile.bat
@@ -0,0 +1,5 @@
+cd C:\Users\elfad\IdeaProjects\ARXMLContainerSorter\src
+javac ARXMLContainerSorter.java
+java ARXMLContainerSorter.java arxmlfile.arxml
+java ARXMLContainerSorter.java diffrentfiletype.txt
+java ARXMLContainerSorter.java empty.arxml
